553560f8c60d4c6223d9939ad0af39f778da0886
dolf <rudolfbyker@gmail.com>
2026-05-21 23:32:28 +0200

    Update the unit tests for the schedule CLI.

--- tests/test_schedule_cli.py
+++ tests/test_schedule_cli.py
@@ -29,12 +29,12 @@ class TestScheduleCli(unittest.TestCase):
             actual_completed_process=completed,
             expected_code=0,
             expected_stderr=[],
-            expected_stdout=["Usage: schedule.py [OPTIONS]"],
+            expected_stdout=["Usage: schedule.py [OPTIONS] COMMAND [ARGS]..."],
             stdout_allow_extra=True,
             stdout_order_matters=False,
         )
 
-    def test_no_execution_mode(self) -> None:
+    def test_py_exec(self) -> None:
         with TemporaryDirectory() as tmp_dir_str:
             tmp_dir = Path(tmp_dir_str)
 
@@ -42,26 +42,18 @@ class TestScheduleCli(unittest.TestCase):
                 args=[
                     sys.executable,
                     schedule_script_path,
-                    "--log-level",
-                    "info",
-                    "--hc-ping-key",
-                    "ping-key",
-                    "--hc-manage-key",
-                    "manage-key",
-                    "--hc-timeout",
-                    "1d",
-                    "--hc-grace",
-                    "2d",
-                    "--interval",
-                    "1s",
-                    "--slug",
-                    "test",
+                    "--log-level=info",
+                    "--max-runs=1",
+                    "--name=test",
                     "--cache-dir",
                     str(tmp_dir),
-                    "--success-period",
-                    "1d",
-                    "--failure-period",
-                    "1h",
+                    "py-exec",
+                    "--code",
+                    """\
+from logging import getLogger
+logger = getLogger(__name__)
+logger.info("hello from py-exec")
+""",
                 ],
                 capture_output=True,
                 env={**os.environ, "PYTHONPATH": repo_dir.as_posix()},
@@ -70,16 +62,15 @@ class TestScheduleCli(unittest.TestCase):
             assert_process_result(
                 test=self,
                 actual_completed_process=completed,
-                expected_code=2,
+                expected_code=0,
                 expected_stderr=[
-                    "Error: Either --command or --py-exec must be provided."
+                    "scheduling_util._schedule_cli INFO: hello from py-exec",
+                    "scheduling_util._schedule INFO: Done.",
                 ],
-                stderr_allow_extra=True,
-                stderr_order_matters=False,
                 expected_stdout=[],
             )
 
-    def test_both_execution_modes(self) -> None:
+    def test_click_invoke(self) -> None:
         with TemporaryDirectory() as tmp_dir_str:
             tmp_dir = Path(tmp_dir_str)
 
@@ -87,30 +78,15 @@ class TestScheduleCli(unittest.TestCase):
                 args=[
                     sys.executable,
                     schedule_script_path,
-                    "--log-level",
-                    "info",
-                    "--hc-ping-key",
-                    "ping-key",
-                    "--hc-manage-key",
-                    "manage-key",
-                    "--hc-timeout",
-                    "1d",
-                    "--hc-grace",
-                    "2d",
-                    "--interval",
-                    "1s",
-                    "--slug",
-                    "test",
+                    "--log-level=info",
+                    "--max-runs=1",
+                    "--name=test",
                     "--cache-dir",
                     str(tmp_dir),
-                    "--success-period",
-                    "1d",
-                    "--failure-period",
-                    "1h",
-                    "--command",
-                    "echo hello",
-                    "--py-exec",
-                    "print('hello')",
+                    "click-invoke",
+                    "--command=scheduling_util:hello",
+                    "--name",
+                    "Bertus",
                 ],
                 capture_output=True,
                 env={**os.environ, "PYTHONPATH": repo_dir.as_posix()},
@@ -119,11 +95,179 @@ class TestScheduleCli(unittest.TestCase):
             assert_process_result(
                 test=self,
                 actual_completed_process=completed,
-                expected_code=2,
+                expected_code=0,
                 expected_stderr=[
-                    "Error: Only one of --command or --py-exec may be provided."
+                    "scheduling_util._example_commands INFO: Hello Bertus!",
+                    "scheduling_util._schedule INFO: Done.",
                 ],
-                stderr_allow_extra=True,
-                stderr_order_matters=False,
+                expected_stdout=[],
+            )
+
+    def test_subprocess(self) -> None:
+        with TemporaryDirectory() as tmp_dir_str:
+            tmp_dir = Path(tmp_dir_str)
+
+            completed = run(
+                args=[
+                    sys.executable,
+                    schedule_script_path,
+                    "--log-level=info",
+                    "--max-runs=1",
+                    "--name=test",
+                    "--cache-dir",
+                    str(tmp_dir),
+                    "subprocess",
+                    "echo",
+                    "Hello world",
+                ],
+                capture_output=True,
+                env={**os.environ, "PYTHONPATH": repo_dir.as_posix()},
+            )
+
+            assert_process_result(
+                test=self,
+                actual_completed_process=completed,
+                expected_code=0,
+                expected_stderr=[
+                    "scheduling_util._schedule_cli.subprocess INFO: Hello world",
+                    "scheduling_util._schedule INFO: Done.",
+                ],
+                expected_stdout=[],
+            )
+
+    def test_subprocess__shell(self) -> None:
+        with TemporaryDirectory() as tmp_dir_str:
+            tmp_dir = Path(tmp_dir_str)
+
+            completed = run(
+                args=[
+                    sys.executable,
+                    schedule_script_path,
+                    "--log-level=info",
+                    "--max-runs=1",
+                    "--name=test",
+                    "--cache-dir",
+                    str(tmp_dir),
+                    "subprocess",
+                    "--shell",
+                    """\
+echo "Hello"
+echo "World"
+""",
+                ],
+                capture_output=True,
+                env={**os.environ, "PYTHONPATH": repo_dir.as_posix()},
+            )
+
+            assert_process_result(
+                test=self,
+                actual_completed_process=completed,
+                expected_code=0,
+                expected_stderr=[
+                    "scheduling_util._schedule_cli.subprocess INFO: Hello",
+                    "scheduling_util._schedule_cli.subprocess INFO: World",
+                    "scheduling_util._schedule INFO: Done.",
+                ],
+                expected_stdout=[],
+            )
+
+    def test_subprocess__command_not_found(self) -> None:
+        with TemporaryDirectory() as tmp_dir_str:
+            tmp_dir = Path(tmp_dir_str)
+
+            completed = run(
+                args=[
+                    sys.executable,
+                    schedule_script_path,
+                    "--log-level=info",
+                    "--max-runs=1",
+                    "--name=test",
+                    "--cache-dir",
+                    str(tmp_dir),
+                    "subprocess",
+                    "this-program-does-not-exist",
+                    "some",
+                    "args",
+                ],
+                capture_output=True,
+                env={**os.environ, "PYTHONPATH": repo_dir.as_posix()},
+            )
+
+            assert_process_result(
+                test=self,
+                actual_completed_process=completed,
+                expected_code=0,
+                expected_stderr=[
+                    "scheduling_util._send_errors_to_slack ERROR: "
+                    "[Errno 2] No such file or directory: 'this-program-does-not-exist'",
+                    "scheduling_util._schedule INFO: Done.",
+                ],
+                expected_stdout=[],
+            )
+
+    def test_subprocess__check(self) -> None:
+        with TemporaryDirectory() as tmp_dir_str:
+            tmp_dir = Path(tmp_dir_str)
+
+            completed = run(
+                args=[
+                    sys.executable,
+                    schedule_script_path,
+                    "--log-level=info",
+                    "--max-runs=1",
+                    "--name=test",
+                    "--cache-dir",
+                    str(tmp_dir),
+                    "subprocess",
+                    "false",
+                ],
+                capture_output=True,
+                env={**os.environ, "PYTHONPATH": repo_dir.as_posix()},
+            )
+
+            assert_process_result(
+                test=self,
+                actual_completed_process=completed,
+                expected_code=0,
+                expected_stderr=[
+                    "scheduling_util._send_errors_to_slack ERROR: "
+                    "Command '('false',)' returned non-zero exit status 1.",
+                    "scheduling_util._schedule INFO: Done.",
+                ],
+                expected_stdout=[],
+            )
+
+    def test_subprocess__no_check(self) -> None:
+        with TemporaryDirectory() as tmp_dir_str:
+            tmp_dir = Path(tmp_dir_str)
+
+            completed = run(
+                args=[
+                    sys.executable,
+                    schedule_script_path,
+                    "--log-level=debug",
+                    "--max-runs=1",
+                    "--name=test",
+                    "--cache-dir",
+                    str(tmp_dir),
+                    "subprocess",
+                    "--no-check",
+                    "false",
+                ],
+                capture_output=True,
+                env={**os.environ, "PYTHONPATH": repo_dir.as_posix()},
+            )
+
+            assert_process_result(
+                test=self,
+                actual_completed_process=completed,
+                expected_code=0,
+                expected_stderr=[
+                    "scheduling_util._last_run DEBUG: [test] Checking if it should run …",
+                    "scheduling_util._last_run DEBUG: [test] Started.",
+                    'scheduling_util._schedule_cli.subprocess DEBUG: Starting process: ["false"]',
+                    "scheduling_util._last_run DEBUG: [test] Succeeded.",
+                    "scheduling_util._schedule INFO: Done.",
+                ],
                 expected_stdout=[],
             )
