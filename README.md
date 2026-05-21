287e94051f7a646344f553d7a84c816a3e188e60
dolf <rudolfbyker@gmail.com>
2026-05-21 13:10:18 +0200

    Project skeleton.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Scheduling utilities, written in Python
+
+Python utilities for scheduling tasks in long-running processes, such as backups and health checks.
+
+My use cases for these are specific enough to warrant writing my own utilities,
+but not general enough that I would publish them on PyPI.
+Wheels are available in the GitHub releases.
+
+## Examples
+
+TODO
---
+++ test/test_doctests.py
@@ -0,0 +1,37 @@
+import doctest
+import unittest
+from pathlib import Path
+from typing import Any, List
+from unittest import BaseTestSuite
+
+repo_dir = Path(__file__).parent.parent.resolve()
+
+
+# noinspection PyUnusedLocal
+def load_tests(loader: Any, tests: BaseTestSuite, ignore: Any) -> BaseTestSuite:
+    """
+    See https://docs.python.org/3/library/doctest.html#unittest-api
+    """
+    modules = find_modules_with_doctests()
+    for module in modules:
+        tests.addTests(doctest.DocTestSuite(module))
+    return tests
+
+
+def find_modules_with_doctests() -> List[str]:
+    modules = []
+    skip_n_parts = len(repo_dir.parts)
+    for path in repo_dir.joinpath("src").rglob("*.py"):
+        if path.name == "__init__.py":
+            continue
+
+        module = ".".join(path.parts[skip_n_parts:])
+        module = module[:-3]
+        modules.append(module)
+    return modules
+
+
+if __name__ == "__main__":
+    unittest.main(
+        failfast=True,
+    )
